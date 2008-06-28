d9dd3a2daaa7c70e228c8ec05a42fba8ea479d39
Vasilis Daloukas <bdaloukas>
2008-06-28 09:27:41 +0000

    *** empty log message ***

--- version.php
+++ version.php
@@ -1,15 +1,15 @@
-<?php // $Id: version.php,v 1.1 2008/03/26 17:40:37 arborrow Exp $
+<?php // $Id: version.php,v 1.2 2008/06/28 09:27:41 bdaloukas Exp $
 /**
  * Code fragment to define the version of game
  * This fragment is called by moodle_needs_upgrading() and /admin/index.php
  *
  * @author 
- * @version $Id: version.php,v 1.1 2008/03/26 17:40:37 arborrow Exp $
+ * @version $Id: version.php,v 1.2 2008/06/28 09:27:41 bdaloukas Exp $
  * @package game
  **/
 
-$module->version  = 2008031401;  // The current module version (Date: YYYYMMDDXX)
-$module->release = 'v1.6.8';
+$module->version  = 2008062801;  // The current module version (Date: YYYYMMDDXX)
+$module->release = 'v2'.substr( $module->version, 4);
 $module->cron     = 0;           // Period for cron to check this module (secs)
 
 ?>

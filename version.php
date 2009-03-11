83ac5164c15a73ad7f2e45296ee2241459fcb0e2
Vasilis Daloukas <bdaloukas>
2009-03-11 21:52:15 +0000

    Better random selection2

--- version.php
+++ version.php
@@ -1,14 +1,14 @@
-<?php // $Id: version.php,v 1.19 2009/03/11 21:32:14 bdaloukas Exp $
+<?php // $Id: version.php,v 1.20 2009/03/11 21:52:15 bdaloukas Exp $
 /**
  * Code fragment to define the version of game
  * This fragment is called by moodle_needs_upgrading() and /admin/index.php
  *
  * @author 
- * @version $Id: version.php,v 1.19 2009/03/11 21:32:14 bdaloukas Exp $
+ * @version $Id: version.php,v 1.20 2009/03/11 21:52:15 bdaloukas Exp $
  * @package game
  **/
 
-$module->version  = 2009031111;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2009031112;  // The current module version (Date: YYYYMMDDXX)
 $module->release = 'v2'.substr( $module->version, 4);
 $module->cron     = 0;           // Period for cron to check this module (secs)
 

750bc0c92b52f5f9408c42ec6e247c8ed8887175
Vasilis Daloukas <bdaloukas>
2010-07-26 00:54:46 +0000

    First version for Moodle 2

--- lang/zh_cn_utf8/game.php
+++
@@ -1,5 +0,0 @@
-<?php // $Id: game.php,v 1.3 2010/07/21 21:17:11 bdaloukas Exp $
-
-//locallib.php
-$string[ 'lettersall'] = 'aāáǎà bcd eēéěè fgh iīíǐì jklm  oōóǒò pqrst uūúǔù ǖǘǚǜü wxyz';	//chinese
-
